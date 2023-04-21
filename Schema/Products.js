06614406ff32be94f86b860975ad0e17f916219e
Rishav Singh <rishavrajcrj@gmail.com>
2023-04-21 22:14:39 +0530

    Update Products.js

--- Schema/Products.js
+++ Schema/Products.js
@@ -5,57 +5,59 @@ const mongoose = require("mongoose");
 //    2- stocks (num of products avilable)
 
 const ProductSchema = new mongoose.Schema({
-    name: {
+  name: {
+    type: "string",
+    required: true,
+  },
+  imageSrc: {
+    type: "string",
+    required: true,
+  },
+  des: {
+    type: "string",
+    required: true,
+  },
+  rating: {
+    type: "Number",
+    required: true,
+    default: 0,
+  },
+  NumReview: {
+    type: "Number",
+    required: true,
+    default: 0,
+  },
+  stocks: {
+    type: "Number",
+    required: true,
+    default: 0,
+  },
+  price: {
+    type: "Number",
+    required: true,
+    default: 0,
+  },
+  gender: {
+    type: "string",
+    required: true,
+  },
+  tags: [
+    {
+      name: {
         type: "string",
         required: true,
+      },
     },
-    imageSrc: {
-        type: "string",
-        required: true,
-    },
-    des: {
-        type: "string",
-        required: true,
-    },
-    rating: {
-        type: "Number",
-        required: true,
-        default: 0,
-    },
-    NumReview: {
-        type: "Number",
-        required: true,
-        default: 0,
-    },
-    stocks: {
-        type: "Number",
-        required: true,
-        default: 0,
-    },
-    price: {
-        type: "Number",
-        required: true,
-        default: 0,
-    },
-    gender: {
-        type: "string",
-        required: true,
-    },
-    tags: [{
-        name: {
-            type: "string",
-            required: true,
-        },
-    }, ],
-    sold: {
-        type: "Number",
-        default: 0,
-        // required: true,
-    },
-    date: {
-        type: Date,
-        default: Date.now,
-    },
+  ],
+  sold: {
+    type: "Number",
+    default: 0,
+    // required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
 module.exports = Users = mongoose.model("Product", ProductSchema);
